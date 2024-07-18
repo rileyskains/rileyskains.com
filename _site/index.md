e458b50906b00d420a599f95b376e030b875c048
Riley Skains <24799022+rileyskains@users.noreply.github.com>
2024-07-17 20:29:53 -0700

    Pure 11ty restart test
    Scrubbed old website content, starting fresh with just pure 11ty.

--- _site/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Home
-layout: home
-tags: page
-modified: 2022-01-09 00:00:00
-order: 1
----
--- _site/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Nav Bar for Mobile
-const button = document.querySelector('#menu-button');
-const menu = document.querySelector('#menu');
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
